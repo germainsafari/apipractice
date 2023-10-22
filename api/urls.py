aa9ae7e496fd2dca41dd67f235961125e8bff5ff
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-22 15:24:27 +0200

    add method

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from . import views
 from .serializers import ItemSerializer
 urlpatterns = [
     path('', views.getData),
-
+    path('add/', views.addItem)
 ] 
--- api/views.py
+++ api/views.py
@@ -8,3 +8,10 @@ def getData(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
