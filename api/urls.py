2d3498ad6da608a746dbdae6f0a64a96af28e2c4
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-22 15:36:16 +0200

    delete

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 from .serializers import ItemSerializer
 urlpatterns = [
     path('', views.getData),
-    path('add/', views.addItem)
+    path('add/', views.addItem),
+    path('delete/<int:item_id>/', views.deleteItem, name='delete-item')
 ] 
